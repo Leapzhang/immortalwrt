1b97dcb381fade52dffc038fc49a3f3aa05a3d16
VIKINGYFY <viking.yfy@qq.com>
2024-11-19 00:14:33 +0800

    Update QCA Source

--- package/base-files/files/etc/uci-defaults/991_set-network.sh
+++ package/base-files/files/etc/uci-defaults/991_set-network.sh
@@ -1,24 +1,28 @@
 #!/bin/sh
 
-uci set dhcp.wan.ignore='1'
+uci set dhcp.wan6=dhcp
+uci set dhcp.wan6.interface='wan6'
 uci set dhcp.wan6.ignore='1'
 
-uci set dhcp.lan.force '1'
+uci set dhcp.lan.force='1'
 uci set dhcp.lan.ra='hybrid'
-uci set dhcp.lan.ra_default '1'
-uci set dhcp.lan.ra_slaac='1'
+uci set dhcp.lan.ra_default='1'
 uci set dhcp.lan.max_preferred_lifetime='900'
 uci set dhcp.lan.max_valid_lifetime='1800'
 
-uci del_list dhcp.lan.ra_flags='other-config'
+uci del dhcp.lan.dhcpv6
+uci del dhcp.lan.ra_flags
+uci del dhcp.lan.ra_slaac
 uci add_list dhcp.lan.ra_flags='none'
 
 uci commit dhcp
 /etc/init.d/odhcpd restart
 
+uci set network.wan6.reqaddress='try'
+uci set network.wan6.reqprefix='auto'
 uci set network.lan.ip6assign='64'
 uci set network.lan.ip6ifaceid='eui64'
-uci set network.globals.ula_prefix=''
+uci del network.globals.ula_prefix
 
 uci commit network
 /etc/init.d/network restart
